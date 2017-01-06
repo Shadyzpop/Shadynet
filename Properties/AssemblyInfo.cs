a8901c1954e73910ea7e4b710dc1d2f14719a92d
Shadyzpop <imedevil@yahoo.com>
2017-01-06 21:28:50 +0200

    Added new

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection ;
+using System.Runtime.CompilerServices ;
+using System.Runtime.InteropServices ;
+using System.Resources ;
+
+// Managing Public information about the assembly by means of 
+// Set of attributes. Change these attribute values ​​to modify the information,
+// Associated with an assembly.
+[assembly: AssemblyTitle ( " Shadynet " )]
+[assembly: AssemblyDescription ( " A .net library USED alternative system to the net with the usage of proxies and supported the Multi-the threading. " )]
+[assembly: AssemblyConfiguration ( " " )]
+[assembly: AssemblyCompany ( " Shadyzpop / Shadyz " )]
+[assembly: AssemblyProduct ( " Shadynet " )]
+[assembly: AssemblyCopyright ( " Policy Copyright © Shady <imedevil@yahoo.com> | github.com/shadyzpop " )]
+[assembly: AssemblyTrademark ( " " )]
+[assembly: AssemblyCulture ( " " )]
+
+// ComVisible parameter with a value of FALSE does invisible types in the assembly 
+// To COM-components. If you need to access a type in this assembly from
+// COM, set the attribute value ComVisible TRUE for this type.
+[assembly: ComVisible ( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid ( " 3fd078d0-a043-4e10-86da-40d19b11142b " )]
+
+// Version information for an assembly consists of the following four values:
+//
+// The main version number
+// Minor Version Number 
+// Build Number
+// Issue
+//
+// You can specify all the values ​​or accept the default number of construction and revision number, 
+// Using the "*", as shown below:
+// [Assembly: AssemblyVersion ( "1.0 *.")]
+[assembly: AssemblyVersion("6.3.0.0")]
+[assembly: AssemblyFileVersion("6.3.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
